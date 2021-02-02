8f9a90be7616dc5c225dd4b6e8456d3d3f85f008
bakial <manhtuan88394@gmail.com>
2021-02-02 18:42:53 -0500

    Add animation on Player2 and change h1 based on 2 dices

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//Show the randomness of 2 dices when refresh the page
+
 let randomNumber1 = ~~(Math.random() * 6) + 1;
+let randomNumber2 = ~~(Math.random() * 6) + 1;
 document.querySelectorAll("img")[0].setAttribute("src", "images/dice"+randomNumber1+".png");
+document.querySelectorAll("img")[1].setAttribute("src", "images/dice"+randomNumber2+".png");
+
+//Change h1 into player1 win, player2 win, or draw when there is the difference or not between 2 dices
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player1 wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player2 wins";
+} else {
+    document.querySelector("h1").textContent = "Draw";
+}
